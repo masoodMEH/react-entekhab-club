ba4431d8ef2c9c3227817dddbd9a7e9fb87f8f62
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-10-14 20:47:22 +0330

    implementing alias import and adding svg assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+
+import react from '@vitejs/plugin-react';
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "./src/"),
+      "@/public": path.join(__dirname, "./public/"),
+      "@/components": path.join(__dirname, "./src/components/"),
+      "@/pages": path.join(__dirname, "./src/pages/"),
+      "@/styles": path.join(__dirname, "./src/styles/"),
+      "@/assets": path.join(__dirname, "./src/assets/"),
+      "@/layout": path.join(__dirname, "./src/app/layout/"),
+    },
+  },
+});
